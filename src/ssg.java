a6a55bdede72f62577b39c2bdbdb0046e85e857a
SouravW28 <souravwadaskar482@gmail.com>
2025-09-01 22:53:22 +0530

    Revert "Commit newly added files"
    This reverts commit b471efd5ea5f58e46dc065c0bef77daa9d7d7df2.

--- src/ssg.java
+++
@@ -1,6 +0,0 @@
-public class ssg {
-    public static void main(String[] args) {
-        int a=5+10;
-
-    }
-}
--- src/stringbuilder.java
+++
@@ -1,6 +0,0 @@
-public class stringbuilder {
-    public static void main(String[] args) {
-        StringBuilder sb=new StringBuilder("Riddhi");
-        System.out.println(sb);
-    }
-}
