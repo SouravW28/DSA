588e610dde11417a1a4a0e196702dc5e8bd7d685
SouravW28 <souravwadaskar482@gmail.com>
2025-09-05 00:34:48 +0530

    Stack Implimentation

---
+++ StackImpl.java
@@ -0,0 +1,52 @@
+public class StackImpl {
+    static class Node{
+        int data;
+        stackimp.Node next;
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    static class Stack{
+        static stackimp.Node head;
+
+        public static boolean isEmpty(){
+            return head==null;
+        }
+        public static void push(int data){
+            stackimp.Node newnode=new stackimp.Node(data);
+            if(isEmpty()){
+                head=newnode;
+                return;
+            }
+            newnode.next=head;
+            head=newnode;
+        }
+        public static int pop(){
+            if(isEmpty()){
+                System.out.println("LIST IS EMPTY");
+            }
+            int top=head.data;
+            head=head.next;
+            return top;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("LIST IS EMPTY");
+            }
+            return head.data;
+        }
+
+        public static void main(String[] args) {
+            Stack s=new Stack();
+            s.push(1);
+            s.push(2);
+            s.push(3);
+            while(!s.isEmpty()){
+                System.out.println(s.peek());
+                s.pop();
+            }
+        }
+    }
+}
